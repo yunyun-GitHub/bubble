089963db6274709261da44b6db6d09a82f6d8e06
yunyun-GitHub <57121602+yunyun-GitHub@users.noreply.github.com>
2022-10-30 10:52:06 +0800

    Add files via upload

--- bubble.py
+++ bubble.py
@@ -1,64 +1,19 @@
-import tkinter
-import threading
-import pystray
+import sys
 import time
 import psutil
-from PIL import ImageTk, Image
+from PySide6.QtCore import Qt, QThread, Signal, QCoreApplication
+from PySide6.QtWidgets import QWidget, QApplication, QLabel, QSystemTrayIcon, QMenu
+from PySide6.QtGui import QPixmap, QPainter, QGuiApplication, QCursor, QIcon, QAction
 
 
-class FloatWindow(tkinter.Tk):
-    def __init__(self):
+class UpdateSystemInformation(QThread):
+    def __init__(self, signal):
         super().__init__()
-        self.image_h = 80
-        self.X = self.Y = 0
-        self.canvas = None
-        self.image = self.image_remake("img/default.png")
+        self.main_thread_update_system_information_signal = signal
 
-        self.create_float_window()
-        threading.Thread(target=self.create_system_tray, daemon=True).start()
-        threading.Thread(target=self.update_system_information, daemon=True).start()
-
-    def create_float_window(self):
-        self.geometry(f'{self.image.size[0]}x{self.image.size[1]}-100-200')  # 设置窗口大小适应图片
-        self.overrideredirect(True)  # 去除标题栏
-        self.wm_attributes('-transparentcolor', 'gray')  # 设置窗口透明
-        # self.wm_attributes("-alpha", 0.5)  # 透明度(0.0~1.0)
-        self.wm_attributes("-topmost", True)  # 永远处于顶层
-
-        self.canvas = tkinter.Canvas(self, bg='gray', highlightthickness=0)
-        self.canvas.pack()
-        self.canvas.image = ImageTk.PhotoImage(self.image)
-        self.canvas.create_image(0, 0, anchor='nw', image=self.canvas.image)
-
-        self.canvas.bind("<Button-1>", self.mousedown)  # 鼠标按下绑定函数，决定可以移动窗体
-        self.canvas.bind("<Double-Button-1>", self.hide_window)  # 双击退出
-        self.canvas.bind("<B1-Motion>", self.mousemove)  # 鼠标按下并移动绑定函数，决定窗体移动到新的位置
-
-    def create_system_tray(self):
-        system_tray = pystray.Icon("tray", self.image, "鼠标移动到\n托盘图标上\n展示内容", menu=pystray.Menu(
-            pystray.MenuItem("更多", pystray.Menu(
-                pystray.MenuItem("广告招商中", self.on_clicked),
-                pystray.MenuItem("广告招商中", self.on_clicked),
-            )),
-            pystray.MenuItem("广告招商中", self.on_clicked),
-            pystray.MenuItem(text='唤醒窗口', action=self.on_clicked, default=True, visible=False),
-            pystray.MenuItem("退出", self.on_clicked),
-        ))
-        system_tray.run()
-
-    def on_clicked(self, icon, item):
-        if str(item) == "广告招商中":
-            print("广告招商中")
-        elif str(item) == "唤醒窗口":
-            self.deiconify()
-        elif str(item) == "退出":
-            icon.stop()
-            self.destroy()
-
-    def update_system_information(self):
+    def run(self):
         sent_before = psutil.net_io_counters().bytes_sent  # 已发送的流量
         recv_before = psutil.net_io_counters().bytes_recv  # 已接收的流量
-        internet_speed = self.canvas.create_text(self.image.size[0]/2, self.image_h/2, text="B/s↑\nB/s↓", fill='gray')
         while True:
             time.sleep(1)
             sent_now = psutil.net_io_counters().bytes_sent
@@ -71,7 +26,7 @@ class FloatWindow(tkinter.Tk):
             sent_before = sent_now
             recv_before = recv_now
 
-            self.canvas.itemconfig(internet_speed, text=f"{sent} ↑\n{recv} ↓")
+            self.main_thread_update_system_information_signal.emit(f"{sent} ↑\n{recv} ↓")
 
     @staticmethod
     def conversion(bit):
@@ -83,24 +38,82 @@ class FloatWindow(tkinter.Tk):
             speed = '%sM/s' % round(bit / 1048576, 2)
         return speed.rjust(10, ' ')
 
-    def image_remake(self, image_path):
-        img = Image.open(image_path)
-        if img.mode != "RGBA":
-            img = img.convert("RGBA")
-        w = int(img.size[0] * self.image_h / img.size[1])
-        return img.resize((w, self.image_h))
 
-    def mousedown(self, event):  # 获取鼠标相对于窗体的坐标
-        self.X, self.Y = event.x, event.y
+class FloatWindow(QWidget):
+    update_system_information_signal = Signal(str)
 
-    def mousemove(self, event):
-        self.geometry(f'+{event.x_root - self.X}+{event.y_root - self.Y}')
+    def __init__(self):
+        super().__init__()
+        self.image_size = 180
+        self.image = QPixmap('img/bubble.png').scaled(self.image_size, self.image_size, aspectMode=Qt.KeepAspectRatio)
+        self.default_label = None
+        self.relative_position = None
 
-    def hide_window(self, event):
-        self.withdraw()
-        print("隐藏窗口", event)
+        self.create_float_window()
+        self.update_system_information_signal.connect(self.update_system_information)
+        self.update_system_information_thread = UpdateSystemInformation(self.update_system_information_signal)
+        self.update_system_information_thread.start()
+
+    def create_float_window(self):
+        """创建浮动窗口"""
+        self.resize(self.image.size())  # 设置窗口大小
+        # self.setMask(self.image.mask())  # 设置遮罩
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint | Qt.SplashScreen)  # 设置无边框和置顶窗口样式隐藏任务栏图标
+        self.setAttribute(Qt.WA_TranslucentBackground)  # 使透明效果生效
+        screen = QGuiApplication.primaryScreen().size()  # 获得屏幕的尺寸
+        self.move(screen.width() - 250, screen.height() - 300)  # 调用move移动到右下角位置
+        self.default_label = QLabel("This is a placeholder text", self)
+        self.default_label.setGeometry(30, 85, 68, 26)
+        self.default_label.setStyleSheet("color: #99FF99; ")
+
+    def update_system_information(self, x):
+        self.default_label.setText(x)
+
+    def mouseDoubleClickEvent(self, event):
+        """双击退出"""
+        # app.quit()
+        self.hide()
+
+    def closeEvent(self, event):
+        self.update_system_information_thread.terminate()  # 结束此线程
+        self.update_system_information_thread.wait()  # 等待结束完成
+        if self.update_system_information_thread.isFinished():  # 如果当前线程已经完成工作，则删除
+            del self.update_system_information_thread
+
+    def paintEvent(self, event):
+        """绘制窗口"""
+        paint = QPainter(self)
+        paint.drawPixmap(0, 0, self.image.width(), self.image.height(), self.image)
+
+    def mousePressEvent(self, event):
+        """左键按下"""
+        self.relative_position = QCursor.pos() - self.pos()
+
+    def mouseMoveEvent(self, event):
+        """左键移动"""
+        self.move(QCursor.pos() - self.relative_position)
+
+
+def quit_app():
+    float_window.show()
+    QCoreApplication.instance().quit()
 
 
 if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    # QApplication.setQuitOnLastWindowClosed(False)
     float_window = FloatWindow()
-    float_window.mainloop()
+    float_window.show()
+
+    tp = QSystemTrayIcon(float_window)
+    tp.setIcon(QIcon('img/bubble.png'))
+    a1 = QAction('&显示(Show)', triggered=float_window.show)
+    a2 = QAction('&退出(Exit)', triggered=quit_app)  # 直接退出可以用qApp.quit
+    tpMenu = QMenu()
+    tpMenu.addAction(a1)
+    tpMenu.addAction(a2)
+    tp.setContextMenu(tpMenu)
+    # 不调用show不会显示系统托盘
+    tp.show()
+
+    sys.exit(app.exec())
